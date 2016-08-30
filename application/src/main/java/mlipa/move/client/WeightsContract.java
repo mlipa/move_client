de2cf1c14ab4a005fd8f82d29dfb0d58b7aee9c5
Marcin Lipa <marcin.lipa@protonmail.com>
2016-08-30 23:29:42 +0200

    Add database contracts

---
+++ application/src/main/java/mlipa/move/client/WeightsContract.java
@@ -0,0 +1,22 @@
+package mlipa.move.client;
+
+import android.provider.BaseColumns;
+
+public class WeightsContract {
+    private WeightsContract() {
+    }
+
+    public static class Weights implements BaseColumns {
+        // TODO: CHANGE WEIGHTS NUMBER TO EQUAL QUALITIES NUMBER
+        private final int WEIGHTS_NUMBER = 100;
+        public static final String TABLE_NAME = "weights";
+        public static final String COLUMN_NAME_CLASSIFIER_ID = "classifier_id";
+        public static final String[] COLUMN_NAME_WEIGHT = new String[WEIGHTS_NUMBER];
+
+        private Weights() {
+            for (int i = 0; i < WEIGHTS_NUMBER; i++) {
+                COLUMN_NAME_WEIGHT[i] = "weight_" + String.valueOf(i);
+            }
+        }
+    }
+}
